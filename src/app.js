fd2d82b70accf572930e6b43851e3ac8d0f75fcb
hariharanraja18 <143488671+hariharanraja18@users.noreply.github.com>
2025-07-04 11:36:08 +0530

    exploring get,post,delete api calls

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test', (req, res) => {
-	res.send('Hello from ExpressJs server');
+// app.use('/user', (req, res) => {
+// 	res.send('Hello Hariharan');
+// });
+
+app.get('/user', (req, res) => {
+	res.send({name:"hari",age:22});
 });
 
-app.use('/hello', (req, res) => {
-	res.send('Hello hello how are you?');
+app.post('/user', (req, res) => {
+	res.send('Save in db successfully');
 });
+
+app.delete('/user',(req,res)=>{
+    res.send("User has been delelted successfully");
+})
 
 app.listen(7777, () => {
 	console.log('Server is successfully listening on port 7777 da?');
